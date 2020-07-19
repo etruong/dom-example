8917287bcd2f3b689df4b3fc29b46f6e71ba9906
Elisa <elisat15@uw.edu>
2020-07-19 15:21:32 -0700

    dom example

---
+++ example.js
@@ -0,0 +1,28 @@
+/**
+ * Grabbing Elements:
+ */
+
+// Get By ID
+
+// Get By Selector
+
+// Get Multiple by Selector
+
+/**
+ * Remove DOM Elements
+ */
+
+// Node Removal
+
+// Node Parent Removal
+
+/**
+ * Appending DOM elements
+ */
+
+// At end of parent
+
+
+// Before a specific element (parentNode.insertBefore())
+
+// Replace
